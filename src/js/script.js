b8a1555f3cda01d3cb9f9e54df78874d8a84c485
bohdan443 <bohdan443@gmail.com>
2021-02-21 10:42:46 +0200

    Create adaptive version for 768px screens

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,28 @@
 // @@include('alert.js');
 
+// BURGER MENU
+
+const burger = document.querySelector('.burger'),
+      nav = document.querySelector('.menu__container'),
+      links = document.querySelectorAll('.menu__container li');
+
+function slideMenu() {
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('menu-active');
+        burger.classList.toggle('toggle');
+        
+        links.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `menuLinksFade .5s ease forwards ${index/7 + 0.3}s`
+            }
+        })
+    })
+}
+
+slideMenu();
+
 //  PHONE MASK
 
 let inpTel = document.querySelectorAll('input[type="tel"]');
@@ -57,7 +80,8 @@ new Swiper('.reviews__slider', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
     },
-    // breakpoints: {
-        
-    // },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
 });
